ae4ff3bfe587129a95e9001ed7e5a12e9eb953b0
roadrunner20 <mehak.arneja0123@gmail.com>
2016-03-07 19:00:15 -0500

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tap Me
+//
+//  Created by Itar Mehak on 2016-03-06.
+//  Copyright © 2016 Itar Mehak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
